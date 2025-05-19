0719dbd6a0a279162f5c2a3619dfe8f60c8e0b3c
Muhammad Tayyab <tayyabimo@gmail.com>
2025-05-20 04:02:15 +0500

    "Force redeploy to fix dashboard issue"

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Star Toursim",
-  description: "Star Tourism Club delivers exceptional travel experiences across Pakistan .Catering to families, groups, corporations, and students, we offer meticulously planned itineraries that encompass transportation, accommodations, and culinary delights. Our deep-rooted local knowledge ensures seamless journeys for both domestic and international travelers, allowing you to fully immerse in the beauty of Pakistan.",
+  description: "Star Tourism Club delivers exceptional travel experiences across Pakistan .Catering to families, groups, corporations, and students, we offer meticulously planned itineraries that encompass transportation, accommodations, and culinary delights.Our deep-rooted local knowledge ensures seamless journeys for both domestic and international travelers, allowing you to fully immerse in the beauty of Pakistan.",
 };
 
 export default function RootLayout({ children }) {
